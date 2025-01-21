6e3bc16bf2bbd7d20dadc1bc206121eed9785c7b
Guilherme <guileisme1@gmail.com>
2025-01-21 20:54:39 -0300

    first steps on C

---
+++ c/myfirstprogram.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int n = 15;
+  float f = 3.141516;
+  char nome[] = "Guilherme";
+  printf("Olá, meu nome é %s!", nome);
+  printf("\nfloat: %f", f);
+  printf("\nint: %d\n", n);
+  return 0;
+}
